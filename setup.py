fb2f742705da684ba31561d534a90011e31a6307
Mitesh Shah <mitesh@miteshshah.com>
2020-07-19 02:03:31 +0530

    feat: release genox on pypi

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ with open("README.md", "r") as f:
 
 setup(
     name="genox",
-    version="0.2",
+    version="0.2.0",
     description="genox - extremely simple static site generator",
     license="MIT",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author="oxalorg",
+    url="https://github.com/oxalorg/genox",
     author_email="mitesh@oxal.org",
     py_modules=["genox"],
     entry_points="""
@@ -19,4 +21,5 @@ setup(
         genox=genox:cli
     """,
     install_requires=["markdown2", "pyyaml", "jinja2"],
+    python_requires='>=3.6',
 )

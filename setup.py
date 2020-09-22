6388284e78e7af9e4cb042568de34b927431d5b6
Mitesh Shah <mitesh@miteshshah.com>
2020-09-23 01:29:28 +0530

    release: v0.2.2

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from setuptools import setup
 with open("README.md", "r") as f:
     long_description = f.read()
 
+with open("VERSION.txt", "r") as f:
+    version = f.read()
 
 setup(
     name="genox",
-    version="0.2.1",
+    version=version,
     description="genox - extremely simple static site generator",
     license="MIT",
     long_description=long_description,
